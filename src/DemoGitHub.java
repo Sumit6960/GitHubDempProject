731aa8ab012877c23fd48ecc97b05609787b9a58
sumit <sumit@DESKTOP-6QJNBHC>
2023-07-29 11:19:59 +0530

    MyFirsTCommit

---
+++ src/DemoGitHub.java
@@ -0,0 +1,19 @@
+
+public class DemoGitHub {
+
+	public static void main(String[] args) {
+	int n=65;
+	
+	if(n%2==0)
+		
+	System.out.println("Even");
+	
+	else {
+		System.out.println("odd");
+	}
+	
+	}
+}
+
+
+
